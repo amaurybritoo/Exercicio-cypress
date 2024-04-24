d769e4193f26644a0555dd4c84a098bbbaed4351
Amaury Brito <amaurypb845@gmail.com>
2024-04-24 03:19:13 -0400

    primeiro commit

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+var faker = require('@faker-js/faker')
+
+
+
+
+
+describe('Funcionalidade Cadastro', () => {
+
+    
+    beforeEach(() => {
+        cy.visit('')
+    });
+    
+    it('deve ser feito o cadastro com sucesso', () => {
+    });
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade Login', () => {
+
+    it('deve fazer login com sucesso', () => {
+      
+    });
+
+    it('deve aparece mensagem de erro ao tentar logar', () => {
+      
+    });
+
+
+});
